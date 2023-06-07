88a65f8bf97c4eb3db1b4caca3c779479cea1465
viktorlyt <viktorlyt@gmail.com>
2023-06-07 21:45:56 +0300

    last

--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,13 @@
 export const getLatestNewsFn = async (searchQuery) => {
   const request = await fetch(
-    `http://hn.algolia.com/api/v1/search?query=${searchQuery}&hitsPerPage=10&page=0`
+    `https://hn.algolia.com/api/v1/search?query=${searchQuery}&hitsPerPage=10&page=0`
   );
   return request.json();
 };
 
 export const getPopularNewsFn = async () => {
   const request = await fetch(
-    `http://hn.algolia.com/api/v1/search?query=&hitsPerPage=10&page=0`
+    `https://hn.algolia.com/api/v1/search?query=&hitsPerPage=10&page=0`
   );
   return request.json();
 };
